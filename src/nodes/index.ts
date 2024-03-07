73de4153ac1c466595ce5c9c602c4455ee98a761
Lexus Drumgold <unicornware@flexdevelopment.llc>
2024-03-07 02:43:53 -0500

    feat(nodes): `BooleanLiteral`
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -6,4 +6,5 @@
 
 export type { default as Literal } from './literal'
 export type { default as BigIntLiteral } from './literal-bigint'
+export type { default as BooleanLiteral } from './literal-boolean'
 export type { default as Node } from './node'
---
+++ src/nodes/literal-boolean.ts
@@ -0,0 +1,27 @@
+/**
+ * @file Nodes - BooleanLiteral
+ * @module esast/nodes/BooleanLiteral
+ */
+
+import type { Literal } from '@flex-development/esast'
+
+/**
+ * A literal boolean value.
+ *
+ * @see {@linkcode Literal}
+ *
+ * @extends {Literal}
+ */
+interface BooleanLiteral extends Literal {
+  /**
+   * Node type.
+   */
+  type: 'boolean'
+
+  /**
+   * Plain value.
+   */
+  value: boolean
+}
+
+export type { BooleanLiteral as default }
